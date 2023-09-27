c59eab81bf29c3611b4f57fb18b7a869ba07ea3c
nrep <elvisrugero@gmail.com>
2023-09-27 12:13:46 +0200

    Configure navigation

--- app/_layout.js
+++ app/_layout.js
@@ -1,5 +1,23 @@
 import { Stack } from 'expo-router';
 
 export default function Layout() {
-    return <Stack screenOptions={{headerShown: false}}/>;
+    return (
+        <Stack>
+            <Stack.Screen name='index' options={{ headerShown: false }} />
+            <Stack.Screen name='login' options={{ headerShown: false }} />
+            <Stack.Screen name='signup' options={{ headerShown: false }} />
+            <Stack.Screen name='help' options={{
+                headerStyle: {
+                    backgroundColor: '#478CCA',
+                },
+                headerTintColor: '#F7BB3A',
+                headerTitleStyle: {
+                    fontWeight: 'bold',
+                    color: 'white',
+                    fontSize: 18
+                },
+                headerTitle: "RBC | BAZA"
+            }} />
+        </Stack>
+    );
 }

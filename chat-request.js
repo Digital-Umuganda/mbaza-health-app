8629aaf72bc42c22142583288eb18f36a9a47c91
nrep <elvisrugero@gmail.com>
2023-10-09 10:49:22 +0200

    Create chat request component

---
+++ chat-request.js
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+
+export default function ChatRequest({ content }) {
+    return (
+        <View style={{ alignSelf: 'flex-end', backgroundColor: '#DAECE8', borderRadius: 16, borderTopRightRadius: 0, width: '80%', marginBottom: 20, flex: 1 }}>
+            <View style={{ flexDirection: 'row', borderBottomWidth: 1, borderBottomColor: '#C7DEE5', padding: 10, width: 'auto' }}>
+                <View style={{ flex: 1, marginRight: 10 }}>
+                    <Text style={{ fontSize: 12, color: '#3CAF4A', fontWeight: '700' }}>Yves Honore</Text>
+                </View>
+                <View style={{ flexDirection: 'row' }}>
+                    <Text style={{ color: '#3D576F8E' }}>{new Date().toLocaleDateString()}</Text>
+                </View>
+            </View>
+            <Text selectable={true} style={{ color: '#3D576F', padding: 15, fontWeight: '500', lineHeight: 24 }}>{content?.answer && content.answer}</Text>
+        </View>
+    );
+}

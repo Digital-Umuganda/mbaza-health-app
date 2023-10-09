14a2d709603d8f9a3542b077bcd6ec2c62f9c327
nrep <elvisrugero@gmail.com>
2023-10-09 10:48:51 +0200

    Add props to button component

--- Button.js
+++ Button.js
@@ -1,7 +1,7 @@
 import { router } from "expo-router";
 import { Text, TouchableOpacity, View } from "react-native";
 
-export default function Button({title, backgroundColor, textColor, onPress, underlineText, borderColor, fontSize}) {
+export default function Button({ title, backgroundColor, textColor, onPress, underlineText, borderColor, fontSize }) {
     return (
         <TouchableOpacity onPress={onPress} style={{ backgroundColor, width: '100%', height: 60, borderRadius: 8, justifyContent: 'center', borderColor, borderWidth: borderColor ? 1 : 0 }}>
             <Text style={{ color: textColor, textAlign: 'center', fontSize: fontSize || 22, fontWeight: 'bold', textDecorationLine: underlineText ? 'underline' : 'none' }}>{title}</Text>

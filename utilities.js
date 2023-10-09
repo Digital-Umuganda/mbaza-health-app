198aa1dc197cdf731cc5c3640f5c6942d8cd7215
nrep <elvisrugero@gmail.com>
2023-10-09 10:51:21 +0200

    Add utility variables and functions

---
+++ utilities.js
@@ -0,0 +1,20 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export const url = "https://backend-api.umuganda.digital";
+
+export const storeData = async (key, value) => {
+    try {
+        await AsyncStorage.setItem(key, value);
+    } catch (e) {
+        // saving error
+    }
+};
+
+export const getData = async (key) => {
+    try {
+        const value = await AsyncStorage.getItem(key);
+        return value;
+    } catch (e) {
+        // error reading value
+    }
+};

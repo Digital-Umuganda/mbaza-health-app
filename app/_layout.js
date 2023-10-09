94d5b7c5eee8f601afd9d5ad6beaf221774fac54
nrep <elvisrugero@gmail.com>
2023-10-09 10:51:41 +0200

    Confihure navigation

--- app/_layout.js
+++ app/_layout.js
@@ -18,6 +18,32 @@ export default function Layout() {
                 },
                 headerTitle: "RBC | BAZA"
             }} />
+            <Stack.Screen name='home' options={{
+                headerStyle: {
+                    backgroundColor: '#478CCA',
+                },
+                headerTintColor: '#F7BB3A',
+                headerTitleStyle: {
+                    fontWeight: 'bold',
+                    color: 'white',
+                    fontSize: 18,
+                },
+                headerTitle: "RBC | BAZA",
+                headerBackVisible: false
+            }} />
+            <Stack.Screen name='chat' options={{
+                headerStyle: {
+                    backgroundColor: '#478CCA',
+                },
+                headerTintColor: '#F7BB3A',
+                headerTitleStyle: {
+                    fontWeight: 'bold',
+                    color: 'white',
+                    fontSize: 18,
+                },
+                headerTitle: "RBC | BAZA"
+            }} />
+            <Stack.Screen name='verify' options={{ headerShown: false }} />
         </Stack>
     );
 }

4ec94a4d7c9480e71ba16686c5ea9104810b9dae
nrep <elvisrugero@gmail.com>
2023-10-16 00:18:24 +0200

    Add validation

--- app/login.js
+++ app/login.js
@@ -9,8 +9,18 @@ import axios from 'axios';
 export default function Login() {
     const [phoneNumber, setPhoneNumber] = useState();
     const [pin, setPin] = useState();
+    const [errorBag, setErrorBag] = useState({});
 
     const logIn = () => {
+        let bag = validate();
+
+        if (bag.phoneNumber == true || bag.pin == true) {
+            setErrorBag(bag);
+            return;
+        }
+
+        setErrorBag({});
+
         const data = {
             username: phoneNumber,
             password: pin,
@@ -24,7 +34,7 @@ export default function Login() {
 
         axios.post(`${url}/api/v1/auth/login`, data)
             .then(function (response) {
-                console.log(response);
+                console.log({ response });
                 storeData('access_token', response.data.access_token)
                     .then(() => storeData('phone_number', phoneNumber)
                         .then(() => router.push('/home')))
@@ -34,29 +44,56 @@ export default function Login() {
             });
     }
 
+    const validate = () => {
+        const data = {
+            username: phoneNumber,
+            password: pin,
+        }
+
+        let bag = {};
+
+        if (data?.username == null || typeof (data.username) != "string" && (!(data.username.startsWith('0') || data.username.startsWith('7')) && data.username.length != 10)) {
+            bag.phoneNumber = true;
+        }
+
+        if (data?.password == null || typeof (data.password) != "string" || data.password.length < 5) {
+            bag.pin = true;
+        }
+
+        return bag;
+    }
+
     return (
         <View style={{ flex: 1, alignItems: 'center', paddingHorizontal: 40 }}>
             <Text style={{ color: '#478CCA', fontSize: 36, marginTop: 100 }}>Aho Kwinjirira</Text>
             <Text style={{ color: '#3D576F', fontSize: 20, marginTop: 10, textAlign: 'center' }}>Uzuzamo nimero yawe ya telefoni na PIN ubundi winjire.</Text>
-            <View style={{ display: 'flex', flexDirection: 'row', borderWidth: 1, width: '100%', borderRadius: 8, borderColor: '#478CCA3D', padding: 15, alignItems: 'center', marginBottom: 20, marginTop: 60 }}>
-                <Image source={require('../assets/phone.png')} style={{ height: 22, width: 14 }} />
-                <TextInput
-                    placeholder='Nimero ya Telefoni (078----)'
-                    placeholderTextColor="#3D576F8E"
-                    style={{ fontSize: 14, marginLeft: 10 }}
-                    keyboardType='numeric'
-                    onChangeText={(text) => setPhoneNumber(text)}
-                />
+            <View style={{ marginBottom: 20, marginTop: 60, width: '100%' }}>
+                <View style={{ display: 'flex', flexDirection: 'row', borderWidth: 1, borderRadius: 8, borderColor: '#478CCA3D', padding: 15, alignItems: 'center' }}>
+                    <Image source={require('../assets/phone.png')} style={{ height: 22, width: 14 }} />
+                    <TextInput
+                        placeholder='Nimero ya Telefoni (078----)'
+                        placeholderTextColor="#3D576F8E"
+                        style={{ fontSize: 14, marginLeft: 10 }}
+                        keyboardType='numeric'
+                        onChangeText={(text) => setPhoneNumber(text)}
+                    />
+                </View>
+                {errorBag.phoneNumber == true && <Text style={{ color: 'red', textAlign: 'center' }}>Tangiza 07 kuri nimero. Igomba kuba igizwe n' imibare icumi.</Text>}
             </View>
-            <View style={{ display: 'flex', flexDirection: 'row', borderWidth: 1, width: '100%', borderRadius: 8, borderColor: '#478CCA3D', padding: 15, alignItems: 'center', marginBottom: 20 }}>
-                <Image source={require('../assets/key-line.png')} style={{ height: 17, width: 18 }} />
-                <TextInput
-                    placeholder='PIN (- - - - -)'
-                    placeholderTextColor="#3D576F8E"
-                    style={{ fontSize: 14, marginLeft: 10 }}
-                    keyboardType='numeric'
-                    onChangeText={(text) => setPin(text)}
-                />
+            <View style={{ marginBottom: 20, width: '100%' }}>
+                <View style={{ display: 'flex', flexDirection: 'row', borderWidth: 1, borderRadius: 8, borderColor: '#478CCA3D', padding: 15, alignItems: 'center' }}>
+                    <Image source={require('../assets/key-line.png')} style={{ height: 17, width: 18 }} />
+                    <TextInput
+                        placeholder='PIN (- - - - -)'
+                        placeholderTextColor="#3D576F8E"
+                        style={{ fontSize: 14, marginLeft: 10 }}
+                        keyboardType='numeric'
+                        onChangeText={(text) => setPin(text)}
+                        secureTextEntry={true}
+                    />
+
+                </View>
+                {errorBag.pin == true && <Text style={{ color: 'red', textAlign: 'center' }}>PIN igomba kuba igizwe n' imibare itari munsi y' itanu</Text>}
             </View>
             <Button title="INJIRA" backgroundColor="#478CCA" textColor="white" onPress={() => logIn()} />
             <View style={{ marginTop: 20 }}></View>
--- app/signup.js
+++ app/signup.js
@@ -13,8 +13,18 @@ export default function Signup() {
     const [names, setNames] = useState();
     const [phoneNumber, setPhoneNumber] = useState();
     const [pin, setPin] = useState();
+    const [errorBag, setErrorBag] = useState({});
 
     const signUp = () => {
+        let bag = validate();
+
+        if (bag.phoneNumber == true || bag.pin == true || bag.district == true || bag.names == true) {
+            setErrorBag(bag);
+            return;
+        }
+
+        setErrorBag({});
+
         const data = {
             name: names,
             phone_number: phoneNumber,
@@ -44,11 +54,11 @@ export default function Signup() {
     }
 
     const sendVerificationCode = () => {
-        const phone = phoneNumber;
+        let phone = phoneNumber;
         if (phone.startsWith('0')) {
-            setPhoneNumber(`25${phone}`);
+            phone = `25${phone}`;
         } else if (phone.startsWith('7')) {
-            setPhoneNumber(`250${phone}`);
+            phone = `250${phone}`;
         }
 
         const config = {
@@ -63,7 +73,7 @@ export default function Signup() {
         axios(config)
             .then(function (response) {
                 // console.log(response);
-                storeData('phone_number', phoneNumber)
+                storeData('phone_number', phone)
                     .then(() => router.push('/verify'))
             })
             .catch(function (error) {
@@ -71,54 +81,96 @@ export default function Signup() {
             });
     }
 
+    const validate = () => {
+        const data = {
+            name: names,
+            phone_number: phoneNumber,
+            address: district,
+            password: pin
+        }
+
+        let bag = {};
+
+        if (data?.phone_number == null || typeof (data.phone_number) != "string" && (!(data.phone_number.startsWith('0') || data.phone_number.startsWith('7')) && data.phone_number.length != 10)) {
+            bag.phoneNumber = true;
+        }
+
+        if (data?.password == null || typeof (data.password) != "string" || data.password.length < 5) {
+            bag.pin = true;
+        }
+
+        if (data?.address == null || typeof (data.address) != "string") {
+            bag.district = true;
+        }
+
+        if (data?.name == null || typeof (data.name) != "string" || data.name.length == 0) {
+            bag.names = true;
+        }
+
+        return bag;
+    }
+
     return (
         <KeyboardAwareScrollView>
             <View style={{ flex: 1, alignItems: 'center', paddingHorizontal: 40 }}>
                 <Text style={{ color: '#478CCA', fontSize: 36, marginTop: 100 }}>Kwiyandikisha</Text>
                 <Text style={{ color: '#3D576F', fontSize: 20, marginTop: 10, textAlign: 'center' }}>Duhe amakuru akurikira maze ubashe kwiyandikisha.</Text>
-                <View style={{ display: 'flex', flexDirection: 'row', borderWidth: 1, width: '100%', borderRadius: 8, borderColor: '#478CCA3D', padding: 15, alignItems: 'center', marginBottom: 20, marginTop: 60 }}>
-                    <Image source={require('../assets/person.png')} style={{ height: 17, width: 17 }} />
-                    <TextInput
-                        placeholder='Amazina yanyu'
-                        placeholderTextColor="#3D576F8E"
-                        style={{ fontSize: 14, marginLeft: 10 }}
-                        onChangeText={(text) => setNames(text)}
-                    />
+                <View style={{ marginBottom: 20, marginTop: 60, width: '100%' }}>
+                    <View style={{ display: 'flex', flexDirection: 'row', borderWidth: 1, borderRadius: 8, borderColor: '#478CCA3D', padding: 15, alignItems: 'center' }}>
+                        <Image source={require('../assets/person.png')} style={{ height: 17, width: 17 }} />
+                        <TextInput
+                            placeholder='Amazina yanyu'
+                            placeholderTextColor="#3D576F8E"
+                            style={{ fontSize: 14, marginLeft: 10 }}
+                            onChangeText={(text) => setNames(text)}
+                        />
+                    </View>
+                    {errorBag.names == true && <Text style={{ color: 'red' }}>Uzuzamo amazina yawe.</Text>}
                 </View>
-                <View style={{ display: 'flex', flexDirection: 'row', borderWidth: 1, width: '100%', borderRadius: 8, borderColor: '#478CCA3D', padding: 15, alignItems: 'center', marginBottom: 20 }}>
-                    <Image source={require('../assets/phone.png')} style={{ height: 22, width: 14 }} />
-                    <TextInput
-                        placeholder='Nimero ya Telefoni (078----)'
-                        placeholderTextColor="#3D576F8E"
-                        style={{ fontSize: 14, marginLeft: 10 }}
-                        keyboardType='numeric'
-                        onChangeText={(text) => setPhoneNumber(text)}
-                    />
+                <View style={{ marginBottom: 20, width: '100%' }}>
+                    <View style={{ display: 'flex', flexDirection: 'row', borderWidth: 1, borderRadius: 8, borderColor: '#478CCA3D', padding: 15, alignItems: 'center' }}>
+                        <Image source={require('../assets/phone.png')} style={{ height: 22, width: 14 }} />
+                        <TextInput
+                            placeholder='Nimero ya Telefoni (078----)'
+                            placeholderTextColor="#3D576F8E"
+                            style={{ fontSize: 14, marginLeft: 10 }}
+                            keyboardType='numeric'
+                            onChangeText={(text) => setPhoneNumber(text)}
+                        />
+                    </View>
+                    {errorBag.phoneNumber == true && <Text style={{ color: 'red', textAlign: 'center' }}>Tangiza 07 kuri nimero. Igomba kuba igizwe n' imibare icumi.</Text>}
                 </View>
-                <View style={{ display: 'flex', flexDirection: 'row', borderWidth: 1, width: '100%', borderRadius: 8, borderColor: '#478CCA3D', paddingVertical: 7, paddingHorizontal: 15, alignItems: 'center', marginBottom: 20 }}>
-                    <Image source={require('../assets/location.png')} style={{ height: 20, width: 16 }} />
-                    <SelectDropdown
-                        data={['MUSANZE', 'GICUMBI', 'NYANZA']}
-                        onSelect={(selectedItem, index) => {
-                            console.log({ selectedItem });
-                            setDistrict(selectedItem);
-                        }}
-                        buttonStyle={{ justifyContent: 'center', alignItems: 'center' }}
-                        defaultButtonText='Hitamo aho mutuye'
-                        buttonTextStyle={{ color: "#3D576F" }}
-                    />
+                <View style={{ marginBottom: 20, width: '100%' }}>
+                    <View style={{ display: 'flex', flexDirection: 'row', borderWidth: 1, borderRadius: 8, borderColor: '#478CCA3D', paddingVertical: 7, paddingHorizontal: 15, alignItems: 'center' }}>
+                        <Image source={require('../assets/location.png')} style={{ height: 20, width: 16 }} />
+                        <SelectDropdown
+                            data={['MUSANZE', 'GICUMBI', 'NYANZA']}
+                            onSelect={(selectedItem, index) => {
+                                console.log({ selectedItem });
+                                setDistrict(selectedItem);
+                            }}
+                            buttonStyle={{ justifyContent: 'center', alignItems: 'center' }}
+                            defaultButtonText='Hitamo aho mutuye'
+                            buttonTextStyle={{ color: "#3D576F" }}
+                        />
+                    </View>
+                    {errorBag.district == true && <Text style={{ color: 'red' }}>Hitamo akarere ukoreramo.</Text>}
                 </View>
-                <View style={{ display: 'flex', flexDirection: 'row', borderWidth: 1, width: '100%', borderRadius: 8, borderColor: '#478CCA3D', padding: 15, alignItems: 'center', marginBottom: 20 }}>
-                    <Image source={require('../assets/key-line.png')} style={{ height: 17, width: 18 }} />
-                    <TextInput
-                        placeholder='PIN uzajya winjiriraho'
-                        placeholderTextColor="#3D576F8E"
-                        style={{ fontSize: 14, marginLeft: 10 }}
-                        keyboardType='numeric'
-                        onChangeText={(text) => setPin(text)}
-                    />
+                <View style={{ marginBottom: 20, width: '100%' }}>
+                    <View style={{ display: 'flex', flexDirection: 'row', borderWidth: 1, borderRadius: 8, borderColor: '#478CCA3D', padding: 15, alignItems: 'center' }}>
+                        <Image source={require('../assets/key-line.png')} style={{ height: 17, width: 18 }} />
+                        <TextInput
+                            placeholder='PIN uzajya winjiriraho'
+                            placeholderTextColor="#3D576F8E"
+                            style={{ fontSize: 14, marginLeft: 10 }}
+                            keyboardType='numeric'
+                            onChangeText={(text) => setPin(text)}
+                            secureTextEntry={true}
+                        />
+                    </View>
+                    {errorBag.pin == true && <Text style={{ color: 'red', textAlign: 'center' }}>PIN igomba kuba igizwe n' imibare itari munsi y' itanu</Text>}
                 </View>
-                <Button onPress={() => sendVerificationCode()} title="EMEZA" backgroundColor="#478CCA" textColor="white" />
+                <Button onPress={() => signUp()} title="EMEZA" backgroundColor="#478CCA" textColor="white" />
                 <View style={{ marginTop: 40 }}></View>
                 <Button title="Subira Inyuma" backgroundColor="transparent" textColor="#3D576F" underlineText={true} onPress={() => router.back()} />
                 <StatusBar style="auto" />

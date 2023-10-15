401e5b3d89ed225338e210e0b4c859cb700b2bd5
nrep <elvisrugero@gmail.com>
2023-10-16 01:28:14 +0200

    Add loading functionality to buttons

--- Button.js
+++ Button.js
@@ -1,10 +1,13 @@
 import { router } from "expo-router";
-import { Text, TouchableOpacity, View } from "react-native";
+import { ActivityIndicator, Text, TouchableOpacity, View } from "react-native";
 
-export default function Button({ title, backgroundColor, textColor, onPress, underlineText, borderColor, fontSize }) {
+export default function Button({ title, backgroundColor, textColor, onPress, underlineText, borderColor, fontSize, loading = false }) {
     return (
-        <TouchableOpacity onPress={onPress} style={{ backgroundColor, width: '100%', height: 60, borderRadius: 8, justifyContent: 'center', borderColor, borderWidth: borderColor ? 1 : 0 }}>
-            <Text style={{ color: textColor, textAlign: 'center', fontSize: fontSize || 22, fontWeight: 'bold', textDecorationLine: underlineText ? 'underline' : 'none' }}>{title}</Text>
+        <TouchableOpacity onPress={onPress} style={{ backgroundColor, width: '100%', height: 60, borderRadius: 8, justifyContent: 'center', borderColor, borderWidth: borderColor ? 1 : 0, flexDirection: 'row', paddingHorizontal: 10 }}>
+            <View style={{ flex: 1, justifyContent: 'center' }}>
+                <Text style={{ color: textColor, textAlign: 'center', fontSize: fontSize || 22, fontWeight: 'bold', textDecorationLine: underlineText ? 'underline' : 'none' }}>{title}</Text>
+            </View>
+            <ActivityIndicator color={'#FFFFFF'} animating={loading} />
         </TouchableOpacity>
     )
 }

6e1d6036e972be123dd55d415ba6c9188d2e2022
nrep <elvisrugero@gmail.com>
2023-10-26 13:12:27 +0200

    Fix loading and Child icon implementation

--- Button.js
+++ Button.js
@@ -1,13 +1,37 @@
-import { router } from "expo-router";
 import { ActivityIndicator, Text, TouchableOpacity, View } from "react-native";
+import { Svg, Path } from "react-native-svg";
 
-export default function Button({ title, backgroundColor, textColor, onPress, underlineText, borderColor, fontSize, loading = false }) {
+export default function Button({ title, backgroundColor, textColor, onPress, underlineText, borderColor, fontSize, loading, children }) {
+    console.log({ children })
     return (
-        <TouchableOpacity onPress={onPress} style={{ backgroundColor, width: '100%', height: 60, borderRadius: 8, justifyContent: 'center', borderColor, borderWidth: borderColor ? 1 : 0, flexDirection: 'row', paddingHorizontal: 10 }}>
+        <TouchableOpacity
+            disabled={loading}
+            onPress={onPress}
+            style={{
+                backgroundColor,
+                width: '100%',
+                height: 60,
+                borderRadius: 8,
+                justifyContent: 'center',
+                borderColor,
+                borderWidth: borderColor ? 1 : 0,
+                flexDirection: 'row',
+                paddingHorizontal: 10
+            }}
+        >
             <View style={{ flex: 1, justifyContent: 'center' }}>
-                <Text style={{ color: textColor, textAlign: 'center', fontSize: fontSize || 22, fontWeight: 'bold', textDecorationLine: underlineText ? 'underline' : 'none' }}>{title}</Text>
+                <Text style={{
+                    color: textColor,
+                    textAlign: 'center',
+                    fontSize: fontSize || 22,
+                    fontWeight: 'bold',
+                    textDecorationLine: underlineText ? 'underline' : 'none'
+                }}
+                >
+                    {title}
+                </Text>
             </View>
-            <ActivityIndicator color={'#FFFFFF'} animating={loading} />
+            {loading == true ? <ActivityIndicator color={'#FFFFFF'} animating={loading != null && loading} /> : children}
         </TouchableOpacity>
     )
 }
